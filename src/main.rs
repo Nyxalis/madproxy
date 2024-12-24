06da98aea4b53ef0902079fc37bcfafc18d0af13
Nyxalis <thayagan21@gmail.com>
2024-12-24 15:16:22 +0000

    Did some minor changes to main.rs before development

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = launch_sequence() {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
+}
+
+fn launch_sequence() -> Result<(), Box<dyn std::error::Error>> {
+    const LAUNCH_ASCII: &str = r#"
+ __  __           _ ____                      
+|  \/  | __ _  __| |  _ \ _ __ _____  ___   _ 
+| |\/| |/ _` |/ _` | |_) | '__/ _ \ \/ / | | |
+| |  | | (_| | (_| |  __/| | | (_) >  <| |_| |
+|_|  |_|\__,_|\__,_|_|   |_|  \___/_/\_\\__, |
+                                        |___/ "#;
+
+    println!("{}", LAUNCH_ASCII);
+
+    Ok(())
 }
